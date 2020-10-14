d4542e775b60c5ed60a730f1af038a429530c7b7
sebafm <seba.ffm@gmail.com>
2020-10-14 16:48:27 +0200

    Fügt den Klassen Parlament und Partei weiteren Code an den Klassen hinzu

--- HareNiemeyer.py
+++ HareNiemeyer.py
@@ -3,51 +3,67 @@ def truncate(n, decimals=0):
     multiplier = 10 ** decimals
     return int(n * multiplier) / multiplier
 
-
-class ParlamentsParteien:
-    def __init__(self, parteien=None):
-        self.parteien = parteien
-
-    def add_parlamentsPartei(partei, landtag):
-        if partei.stimmzahl / landtag.anzahl_sitze >= 0.05:
-            self.parteien["partei.name"] = partei
-
-    def get_parlamentsParteien():
-        return self.parteien
-
-class Landtag:
-    def __init__(self, gesamtzahl_stimmen, parlamentsparteien=ParlamentsParteien, anzahl_sitze=110):
-        self.parlamentsparteien = parlamentsparteien
+#########################################
+class Parlament:
+    """Speichert alle Parteien, die die 5%-Hürde genommen haben, in einem Dictionary und brechnet deren Sitze, Überhangmandate, etc."""
+    def __init__(self, gesamtzahl_stimmen=0, parteien=dict(), anzahl_sitze=110):
         self.gesamtzahl_stimmen = gesamtzahl_stimmen
+        self.parteien = parteien
         self.anzahl_sitze = anzahl_sitze
-    
-    def sitzanteil(self, partei):
-        return partei.stimmzahl * self.anzahl_sitze / self.gesamtzahl_stimmen
-    
-    def belegte_sitze_nach_vollen_zahlen(self, partei):
-        return truncate(self.sitzanteil(partei))
 
-    def nachkommastellen(self, partei):
-        return self.sitzanteil(partei) - self.belegte_sitze_nach_vollen_zahlen(partei)
-    
-    def sitze_verbleibend_fuer_nachkommastellen():
-        pass
+    def fuege_parlamentsPartei_hinzu(self, partei):
+        if partei.stimmzahl / landesstimmen >= 0.05:
+            self.parteien[partei.name] = partei   
+            a = partei.stimmzahl * self.anzahl_sitze / self.gesamtzahl_stimmen
+            self.parteien[partei.name].quote = a
+            self.parteien[partei.name].sitze_volle_zahl = truncate(a)
 
-    def zuordnung_sitze_nachkommastellen():
-        pass
+    def set_direktmandate(self, parteiname, direktmandate):
+        self.parteien[parteiname].direktmandate = direktmandate
+    
+    def sitzverteilung_nachkommastellen(self, sitze_verbleibend_fuer_nachkommastellen):
+        a = list()
+        for k,v in self.parteien.items():
+            a.append(((v.quote - truncate(v.quote)), self.parteien[k].name))
+        b = sorted(a, reverse=True)
+        for val, key in b[:sitze_verbleibend_fuer_nachkommastellen]:
+            self.parteien[key].sitze_nachkommastellen = 1
+            #print(self.parteien[key].name, self.parteien[key].sitze_nachkommastellen)
+
+        
+#########################################
 
 
 class Partei:
-    def __init__(self, name, stimmzahl, direktmandate=0, ueberhangmandate=0, sitze_volle_zahl=0, nachkommastellen = 0.0):
+    def __init__(self, name, stimmzahl, quote=0, direktmandate=0, ueberhangmandate=0, sitze_volle_zahl=0, \
+                nachkommastellen = 0.0, sitze_nachkommastellen=0, ausgleichsmandate=0):
         self.name = name
         self.stimmzahl = stimmzahl
+        self.quote = quote
         self.direktmandate = direktmandate
         self.ueberhangmandate = ueberhangmandate
         self.sitze_volle_zahl = sitze_volle_zahl
         self.nachkommastellen = nachkommastellen
+        self.sitze_nachkommastellen = sitze_nachkommastellen
+        self.ausgleichsmandate = ausgleichsmandate
+
+    def __str__(self):
+        return ("Name: {}, \n\tStimmzahl: {}, \n\tQuote: {}, \n\tSitze_volle_Zahl: {}, \n\tSitze_Nachkommastellen: {}, "\
+                "\n\tDirektmandate: {}, \n\tÜberhangmandate: {}, \n\tAusgleichsmandate: {}".format(self.name, self.stimmzahl, self.quote, \
+                self.sitze_volle_zahl, self.sitze_nachkommastellen, self.direktmandate, self.ueberhangmandate, self.ausgleichsmandate))
+
+###############################################################################################
+
+class Wahl:
+    """Beinhaltet alle Eckdaten einer Wahl"""
+    def __init__(self, landesstimmen, gesamtstimmenpool=dict()):
+        self.landesstimmen = landesstimmen # Anzahl der abgegebenen Stimmen bei der Wahl
+        self.gesamtstimmenpool = gesamtstimmenpool
 
 
+################################################################################################
 
+landesstimmen = 2881261        
 gesamtstimmenpool = {
     "CDU": 776910,
     "SPD": 570446,
@@ -55,16 +71,22 @@ gesamtstimmenpool = {
     "LINKE": 181332,
     "FDP": 215946,
     "AfD": 378692,
+    "Graue_Panther": 25352,
+    "Oeko_Linx": 32457
 }
 
-parlamentsParteien = []
-for k,v in gesamtstimmenpool.items():
-    parlamentsParteien.append(Partei(k, v))
+#wahl = Wahl(landesstimmen, gesamtstimmenpool)
+parlament = Parlament(2693838)
 
-# cdu = Partei("CDU", 776910, 40)
-# landtag = Landtag(2693838)
-# sitzRechner = SitzRechner(landtag)
-# for i in range(len(parlamentsParteien)):
-#     print(sitzRechner.belegte_sitze_nach_vollen_zahlen(parlamentsParteien[i]))
-# for i in range(len(parlamentsParteien)):
-#     print(sitzRechner.nachkommastellen(parlamentsParteien[i]))
+#Fülle die Parlament-Klasenvariable "parteien" mit denjenigen Parteien, die ins Parlament einziehen
+for k,v in gesamtstimmenpool.items():
+    parlament.fuege_parlamentsPartei_hinzu(Partei(k, v))
+            
+
+parlament.set_direktmandate("CDU", 40)
+parlament.set_direktmandate("SPD", 10)
+parlament.set_direktmandate("GRUENE", 5)
+parlament.sitzverteilung_nachkommastellen(3)
+for k,v in parlament.parteien.items():
+    print(v)
+
