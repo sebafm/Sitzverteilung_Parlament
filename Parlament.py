1f30944dabb83ed1bbb59d6974eb73635318d63c
sebafm <seba.ffm@gmail.com>
2020-10-26 16:49:26 +0100

    Method disambiguation, main function outsourced to main.py

--- Parlament.py
+++ Parlament.py
@@ -1,4 +1,6 @@
 import math
+import pandas as pd
+import myMathFunctions
 
 #####################################################################
 # Eine beliebige Partei:
@@ -58,9 +60,9 @@ class Landtag:
         for p in range(len(self.parteien)):
             self.parteien[p].sitze_nach_voller_zahl = math.floor(self.parteien[p].quote)
 
-    def berechne_zu_verteilende_nachkommasitze(self):
-        """Berechnet die Anzahl nicht vergebener Sitze, die nach der Sitzzuweisung an 
-        die Parteien anhand der vollen Zahlen ihrer jew. Quote verbleiben.
+    def get_nicht_verteilte_restsitze(self):
+        """Berechnet die Anzahl der Sitze, die nach der Sitzzuweisung an 
+        die Parteien anhand der vollen Zahlen ihrer jew. Quote unverteilt geblieben sind.
         ==> zu_verteilende_nachkommasitze : int"""
         vergebene_sitze = 0
         for p in self.parteien:
@@ -78,40 +80,33 @@ class Landtag:
             lst.append((nkstell, name))
         return sorted(lst, reverse=True)
     
-    def assign_sitze_nachkommastellen(self):
+    def assign_restsitze(self):
         """Verteilt die verbleibenden Sitze (zu_verteilende_nachkommasitze) an die Parteien 
         in parteien mit den höchsten Nachkommastellen."""
-        index_bis = self.berechne_zu_verteilende_nachkommasitze()
+        index_bis = self.get_nicht_verteilte_restsitze()
         parteienliste = self.ordne_parteien_by_nachkommastellen()
         for p in parteienliste[:index_bis]:
             for i in range(len(self.parteien)):
                 if p[1] == self.parteien[i].name:
                     self.parteien[i].sitz_anhand_nachkommastellen = 1
 
+    def finde_partei_mit_meisten_ueberhangmandaten(self):
+        biggest_ueberhang = 0
+        name = ""
+        quote = 0
+        for partei in self.parteien:
+            ueberhang = partei.direktmandate - partei.quote
+            if ueberhang > biggest_ueberhang:
+                name = partei.name
+                biggest_ueberhang = ueberhang
+                quote = partei.quote
+        return (name, biggest_ueberhang, quote)
+
+    def determine_seats_by_ueberhangmandate(self):
+        """Betimmt die neue Anzahl der Sitze des Landtags, ausgehend von den Ueberhangmandaten
+        und den entsprechend den anderen Parteien zustehenden Ausgleichsmandaten."""
+        ausschlaggebende_partei = self.finde_partei_mit_meisten_ueberhangmandaten()
+        neue_sitzanzahl = ausschlaggebende_partei[1] * 100 / ausschlaggebende_partei[2]
+        min_max_sitzanzahl = [ausschlaggebende_partei[0], math.floor(neue_sitzanzahl) + self.sitze , math.ceil(neue_sitzanzahl) + self.sitze ]
+        return min_max_sitzanzahl
 #######################################################################
-
-def main():
-    #"""Ergebnisse der Wahl von 2018"""
-    landesstimmen = 2881261        
-    gesamtstimmenpool = [
-        ("CDU", 776910, 40),
-        ("SPD", 570446, 10),
-        ("GRÜNE", 570512, 5),
-        ("LINKE", 181332, 0),
-        ("FDP", 215946, 0),
-        ("AfD", 378692, 0),
-        ("Graue_Panther", 25352, 0),
-        ("Oeko_Linx", 32457, 0)
-    ]
-
-    hlt = Landtag(landesstimmen)
-    hlt.bestimme_parlamentsparteien(gesamtstimmenpool)
-    hlt.berechne_quoten_parteien()
-    hlt.berechne_sitze_volle_zahl()
-    hlt.assign_sitze_nachkommastellen()
-    
-    for p in hlt.parteien:
-        print(p)
-
-
-if __name__ == "__main__": main() 
---
+++ main.py
@@ -0,0 +1,54 @@
+from Parlament import Landtag
+import pandas as pd
+import myMathFunctions
+
+def main():
+    #"""Ergebnisse der Wahl von 2018"""
+    landesstimmen = 2881261        
+    gesamtstimmenpool = [
+        ("CDU", 776910, 40),
+        ("SPD", 570446, 10),
+        ("GRÜNE", 570512, 5),
+        ("LINKE", 181332, 0),
+        ("FDP", 215946, 0),
+        ("AfD", 378692, 0),
+        ("Graue_Panther", 25352, 0),
+        ("Oeko_Linx", 32457, 0)
+    ]
+
+    #Berechnung des "Standard"-HLT mit 110 Sitzen
+    hlt = Landtag(landesstimmen)
+    hlt.bestimme_parlamentsparteien(gesamtstimmenpool)
+    hlt.berechne_quoten_parteien()
+    hlt.berechne_sitze_volle_zahl()
+    hlt.assign_restsitze()
+    
+    #Bestimme zwei mögliche Größen für die Gesamtsitze des Landtags anhand 
+    #der Überhangmandate
+    min_max_sitze = hlt.determine_seats_by_ueberhangmandate()
+
+    #Berechnung des HLT mit Überhangmandaten: die Max-Variante
+    hltMin = Landtag(landesstimmen, min_max_sitze[1])
+    hltMin.bestimme_parlamentsparteien(gesamtstimmenpool)
+    hltMin.berechne_quoten_parteien()
+    hltMin.berechne_sitze_volle_zahl()
+    hltMin.assign_restsitze()
+    # for p in hltMin.parteien:
+    #     print(p)
+
+    data = [vars(p) for p in hltMin.parteien]
+    print(pd.DataFrame.from_dict(data))
+
+    hltMax = Landtag(landesstimmen, min_max_sitze[2])
+    hltMax.bestimme_parlamentsparteien(gesamtstimmenpool)
+    hltMax.berechne_quoten_parteien()
+    hltMax.berechne_sitze_volle_zahl()
+    hltMax.assign_restsitze()
+    # for p in hltMax.parteien:
+    #     print(p)
+
+    data = [vars(p) for p in hltMax.parteien]
+    print(pd.DataFrame.from_dict(data))
+
+
+if __name__ == "__main__": main() 
