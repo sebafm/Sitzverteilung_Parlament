b4449c8533efd785f5a3f90d9cd57e3a16ea3351
sebafm <seba.ffm@gmail.com>
2020-10-26 16:52:56 +0100

    Deletes temp file "DataFrame_Uebung.py"

--- DataFrame_Uebung.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from HareNiemeyer import *
-
-data = dict()
-for k,v in Parlament.parteien.items():
-    data[k] = [v.name, v.stimmzahl. v.quote]
-
-print(pd.DataFrame.from_dict(data))
-
